10af67eb8917e37e5eb48b18cc92d027b9d37449
Loïc Dutrieux <loic.dutrieux@gmail.com>
2016-07-27 23:04:42 +0200

    add cairo to installed packages

--- install.sh
+++ install.sh
@@ -36,6 +36,7 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install r-base r-base-dev
+sudo apt-get install libcairo-dev
 
 sudo apt-get install gdebi-core
 wget https://download1.rstudio.org/rstudio-0.99.903-amd64.deb
