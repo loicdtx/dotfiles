586f7099f59aac9d5096a437b0f1b317aa2b491e
Loïc Dutrieux <loic.dutrieux@gmail.com>
2016-08-05 22:20:06 +0200

    fixed postgis installation

--- install.sh
+++ install.sh
@@ -58,5 +58,5 @@ sudo apt-get install dropbox
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-9.3 pgadmin3 postgresql-contrib
-sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib
+sudo apt-get install postgresql-9.5 pgadmin3 postgresql-contrib
+sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgis
